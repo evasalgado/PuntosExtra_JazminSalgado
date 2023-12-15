ec137ff6dd118bff252b78f4bb752fc8ddb55597
evasalgado <imviarsha@gmail.com>
2023-12-15 14:04:08 -0600

    aun más cosas jaja

--- PuntosExtra_JazminSalgado/src/puntosextra_jazminsalgado/deporte.java
+++ PuntosExtra_JazminSalgado/src/puntosextra_jazminsalgado/deporte.java
@@ -4,10 +4,43 @@
  */
 package puntosextra_jazminsalgado;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author evaja
  */
 public class deporte {
+    private ArrayList<torneo> torneos = new ArrayList<>();
+    private int periodo;
+
+    public deporte() {
+    }
+
+    public deporte(int periodo) {
+        this.periodo = periodo;
+    }
+
+    public ArrayList<torneo> getTorneos() {
+        return torneos;
+    }
+
+    public void setTorneos(ArrayList<torneo> torneos) {
+        this.torneos = torneos;
+    }
+
+    public int getPeriodo() {
+        return periodo;
+    }
+
+    public void setPeriodo(int periodo) {
+        this.periodo = periodo;
+    }
+
+    @Override
+    public String toString() {
+        return "deporte{" + "torneos=" + torneos + ", periodo=" + periodo + '}';
+    }
+    
     
 }
