e09e40f41fc877b862abbc30ea3fd8ccc8050176
evasalgado <imviarsha@gmail.com>
2023-12-15 14:02:16 -0600

    agrego mas cosas

--- PuntosExtra_JazminSalgado/src/puntosextra_jazminsalgado/equipo.java
+++ PuntosExtra_JazminSalgado/src/puntosextra_jazminsalgado/equipo.java
@@ -9,5 +9,37 @@ package puntosextra_jazminsalgado;
  * @author evaja
  */
 public class equipo {
+    private String nombre;
+    private int puntos;
+
+    public equipo() {
+    }
+
+    public equipo(String nombre, int puntos) {
+        this.nombre = nombre;
+        this.puntos = puntos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+
+    @Override
+    public String toString() {
+        return   nombre ;
+    }
+    
     
 }
